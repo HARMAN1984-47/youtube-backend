2d7154fed6f7c69f8a1799ebe3201ea8463984a7
harman <harmann967@gmail.com>
2026-04-29 16:18:17 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("servern is running at port : " , process.env.PORT);
+        app.on("error",(error) => {
+            console.log("error",error);
+            throw error
+        })
+        
+    })
+})
+.catch((err) =>{
+    console.log("mongo db connection failed!!!",err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHander) => {
+    (req,res,next) => {
+        Promise.resolve(requestHander(req,res,next)).catch((err)  => next(err))
+    }
+}
+
+export { asyncHandler };
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 5000).json({
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// };
